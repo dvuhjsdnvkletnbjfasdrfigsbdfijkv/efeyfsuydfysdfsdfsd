8c52b68aa649b7ed8d6e4a8a6bd299bf3a87b19f
sdkjwjehfbsdamadbffghdfggetjhrrhryujdfd <110531833+sdkjwjehfbsdamadbffghdfggetjhrrhryujdfd@users.noreply.github.com>
2022-09-12 07:01:48 +0430

    Update help.js

--- SlashCommands/Info/help.js
+++ SlashCommands/Info/help.js
@@ -13,11 +13,11 @@ module.exports = {
         \`\`\`elm
         Help
         list command haye bot
-        \`\`\`
+        \`\`\`elm
         \`\`\`elm
         Stats
         moshahede stats bot
-        \`\`\`
+        \`\`\`elm
         \`\`\`elm
         Invite
         link invite bot
@@ -25,24 +25,24 @@ module.exports = {
         Karma
         link invite karma gif
         link invite karma porn
-        \`\`\`
+        \`\`\`elm
         > **Command haye ticket**
         \`\`\`elm
         Ticket-setup
         setup kardan ticket system
-        \`\`\`
+        \`\`\`elm
         \`\`\`elm
         Ticket-logs
         setup kardan log system
-        \`\`\`
+        \`\`\`elm
         > **𝐊 Δ 𝐑 𝐌 Δ Developer Commands**
         \`\`\`elm
         Allservers
         moshahede ems server hayi ke bot dakheleshon hast
-        \`\`\`
+        \`\`\`elm
         Manage_bot
         manage kardan bot, change dadan esm bot, change dadan prof bot, shutdown kardan bot
-        \`\`\``)
+        \`\`\`elm`)
         .setImage(`https://cdn.discordapp.com/attachments/987778608401621002/1018206528169320569/20220910_213822.gif`) 
       .setFooter({text:`𝐊 Δ 𝐑 𝐌 Δ`, iconURL: ('https://cdn.discordapp.com/attachments/987778608401621002/1018206529289195660/20220910_213653.gif')})
       return interaction.followUp({ embeds: [embed] })
